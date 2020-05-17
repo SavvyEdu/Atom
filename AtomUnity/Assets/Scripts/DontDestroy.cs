63b6aeebd41f7771b74b299836c72c46d6d60d8e
Dane Sherman <danemsherman@gmail.com>
2020-05-16 23:56:53 -0400

    Background Audio

---
+++ AtomUnity/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    //tracks objects that area in "Don't Destroy"
+    private static List<string> objects;
+
+    static DontDestroy(){
+        objects = new List<string>();
+    }
+
+    private void Awake()
+    {
+        if (!objects.Contains(gameObject.name)){
+            objects.Add(gameObject.name);
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
