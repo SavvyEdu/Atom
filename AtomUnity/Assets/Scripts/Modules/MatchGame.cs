ee26cffe8b7d86d80854fe128f03d11c6a381775
Dane Sherman <danemsherman@gmail.com>
2021-01-13 00:09:16 -0500

    Match Game resetting and win/lose

--- AtomUnity/Assets/Scripts/Modules/MatchGame.cs
+++ AtomUnity/Assets/Scripts/Modules/MatchGame.cs
@@ -7,6 +7,7 @@ using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 using Atom;
+using Atom.Util;
 
 public class MatchGame : MonoBehaviour
 {
@@ -15,26 +16,73 @@ public class MatchGame : MonoBehaviour
     [SerializeField] private Atom.Atom atom;
     [SerializeField] private AtomicSymbol targetSymbol;
     [SerializeField] private AtomicSymbol atomSymbol;
-    [SerializeField] private GameObject tutorialUI;
+    
+    [Header("Effects")]
+    [SerializeField] private GameObject stars;
+    private SVGImage[] starImages;
     [SerializeField] private SpriteRenderer glowEffect;
+    private bool glowAnimating = false;
+    [SerializeField] private Text timerText;
 
-    int targetProtons, targetNeutrons, targetElectrons;
-    bool completed = false;
+    [Header("Panel")]
+    [SerializeField] private GameObject tutorialUI;
+    [SerializeField] private Text message;
+    [SerializeField] private Text title;
+    [TextArea] [SerializeField] private string tutorialText;
+    [TextArea] [SerializeField] private string winText;
+    [TextArea] [SerializeField] private string loseText;
+
+    private int targetProtons = 1, targetNeutrons = 0, targetElectrons = 1;
+    private int numCompleted = 0;
+    private bool challenge = false;
 
     private AudioSource audioSource;
 
     private void Awake()
     {
         audioSource = GetComponent<AudioSource>();
+        starImages = stars.GetComponentsInChildren<SVGImage>();
 
         glowEffect.gameObject.SetActive(false); //hide the gloweffect
+        
         tutorialUI.gameObject.SetActive(true); //show the tutorial
+        title.text = "SYMBOL MATCH";
+        message.text = tutorialText;
 
         //make the text color for target symbol blue
         targetSymbol.SetUIColors(UIColors.blue, true, true, true, true, true);
 
-        SetRandomTarget();
+        atom.Interactable = false;
     }
+
+    private void Start()
+    {
+        ResetGame();
+    }
+
+    public void StartGame()
+    {
+        atom.Interactable = true;
+        StartCoroutine("Coutdown");
+    }
+
+    private void ResetGame()
+    {
+        //remove atom particles
+        atom.ForceToCommon(1);
+
+        //reset targets
+        targetProtons = 1;
+        SetRandomTarget();
+
+        //reset stars
+        numCompleted = 0;
+        foreach (SVGImage star in starImages)
+        {
+            star.color = UIColors.lightGrey;
+        }
+    }
+
     private void Update()
     {
         //check for each of the 3 particle counts
@@ -52,16 +100,54 @@ public class MatchGame : MonoBehaviour
             charge: eMatch);
 
         //check for all matching
-        if (!completed && pMatch && nMatch && eMatch) 
+        if (!glowAnimating && pMatch && nMatch && eMatch) 
         {
-            completed = true;
+            glowAnimating = true;
 
+            starImages[numCompleted].color = Color.yellow;
+            numCompleted++;
+            if(numCompleted == stars.transform.childCount)
+            {
+                StopCoroutine("Coutdown"); //stop the coutdown
+                challenge = true;
+                ResetGame();
+
+                //display the win message 
+                tutorialUI.gameObject.SetActive(true); //show the tutorial
+                title.text = "Nice Work!";
+                message.text = winText;
+                atom.Interactable = false;
+            }
+            
             // Play the completed SFX
             audioSource.Play();
 
             //Visual glow then Make a new target after
             StartCoroutine(GlowFX(audioSource.clip.length, SetRandomTarget));
         }
+    }
+
+    private IEnumerator Coutdown()
+    {
+        int t = challenge ? 60 : 12;
+        while(t > 0)
+        {
+            yield return new WaitForSeconds(1);
+            t--;
+
+            int second = t % 60;
+            if(second < 10) 
+                timerText.text = $"{t / 60}:0{second}";
+            else
+                timerText.text = $"{t / 60}:{second}";
+        }
+        ResetGame();
+
+        //display the win message 
+        tutorialUI.gameObject.SetActive(true); //show the tutorial
+        title.text = "Out of Time";
+        message.text = loseText;
+        atom.Interactable = false;
     }
 
     private IEnumerator GlowFX(float time, Action callback = null)
@@ -88,14 +174,26 @@ public class MatchGame : MonoBehaviour
 
     private void SetRandomTarget()
     {
-        completed = false;
+        glowAnimating = false;
 
-        targetProtons = Random.Range(0, 18) + 1;
+        //determine target protons (randomly increase the count)
+        targetProtons = Random.Range(targetProtons + 2, targetProtons + 5);
         Element targetElement = Elements.GetElement(targetProtons);
 
+        //determine target neutrons (use common element)
         int mass = targetElement.Common.Mass;
         targetNeutrons = mass - targetProtons;
+
+        //determine target electrons
         targetElectrons = targetProtons;
+        Debug.Log(targetElement.Block);
+        switch (targetElement.Block)
+        {
+            case BlockType.sBlock:
+                targetElectrons += Random.Range(0, 2); break;
+            case BlockType.pBlock:
+                targetElectrons -= Random.Range(0, 3); break;
+        }
 
         targetSymbol.SetSymbol(targetElement, targetProtons, mass, targetElectrons);
     }
