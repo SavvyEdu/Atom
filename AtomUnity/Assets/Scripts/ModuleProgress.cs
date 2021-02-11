eb8387271e8e586b172a3cd7cb57f63f0416dfce
Dane Sherman <danemsherman@gmail.com>
2021-02-10 23:56:51 -0500

    Inline added to all Learn Scenes

---
+++ AtomUnity/Assets/Scripts/ModuleProgress.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModuleProgress : MonoBehaviour
+{
+    //gameobject is part of the main scene, contains key and GameObject
+    [SerializeField] private ModuleData[] moduleData;
+
+    private void Awake()
+    {
+        foreach (ModuleData data in moduleData)
+        {
+            //PlayerPrefs.GetInt(data.key, 0) == 1;
+            
+        }
+    }
+
+
+    //TODO: should probably loop over keys 
+    //TODO: hook up to settings panel UI 
+    public void EraseAllProgress() {
+        PlayerPrefs.DeleteAll();
+    }
+}
+
+
+public struct ModuleData
+{
+    public string key;
+    public GameObject UI;
+    public bool hasBonus;
+}
