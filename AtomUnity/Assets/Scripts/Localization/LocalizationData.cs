c33e0ba2e9e1e75f4e982be5d51456a12b5e27f9
Dane Sherman <danemsherman@gmail.com>
2021-03-01 15:11:04 -0500

    Localization Scene and Data setup

---
+++ AtomUnity/Assets/Scripts/Localization/LocalizationData.cs
@@ -0,0 +1,18 @@
+﻿namespace Localization
+{
+    /// <summary>
+    /// Serializable array of dictionary items
+    /// </summary>
+    [System.Serializable]
+    public class LocalizationData
+    {
+        public LocalizationItem[] items;
+    }
+
+    [System.Serializable]
+    public class LocalizationItem
+    {
+        public string key;
+        public string value;
+    }
+}
---
+++ AtomUnity/Assets/Scripts/Localization/LocalizationManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+namespace Localization
+{
+    public class LocalizationManager : MonoBehaviour
+    {
+        private Dictionary<string, string> localizedText;
+        public bool isLoaded { get; private set; } = false;
+
+        public void LoadLocalizedText(string fileName)
+        {
+            localizedText = new Dictionary<string, string>();
+            string filePath = Path.Combine(Application.streamingAssetsPath, fileName);
+
+            //make sure data actually exists
+            if (File.Exists(filePath))
+            {
+                //Load in JSON
+                string dataAsJson = File.ReadAllText(filePath);
+                LocalizationData loadedData = JsonUtility.FromJson<LocalizationData>(dataAsJson);
+
+                //Convert to LoacalizedText Dictionary
+                foreach(var item in loadedData.items)
+                {
+                    localizedText.Add(item.key, item.value);
+                }
+
+                Debug.Log($"Data loaded from {fileName}. Dictionary contains: {localizedText.Count} entries");
+            }
+            else
+            {
+                Debug.LogError("Cannot find file!");
+            }
+
+            isLoaded = true;
+        }
+    }
+}
